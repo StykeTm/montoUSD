e72295547f16e83ae8b8d33bc04544e95a35e3d0
styketm <jony.jimenezz@gmail.com>
2024-08-29 16:53:17 -0400

    Eliminado comentario

--- calcuint.js
+++ calcuint.js
@@ -17,6 +17,3 @@ console.log(`El valor que tú debes es ${montoMio()}`);
 console.log(`El valor que deben pagar es ${montoAmbos()}`);
 console.log(`El valor que debe para Su es ${montoSu()}`);
 
-/* Script en JS para calcular lo que corresponde pagar a cada uno de la parte internacional
-debe ingresar los valores solicitados y para ser ejecutados en la terminal
-del directorio o acá en VSCode ingresar "node calcuint.js"*/
